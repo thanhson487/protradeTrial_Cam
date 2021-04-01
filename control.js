4c4629d31ee07493bcf7155c30b6072e6a823678
thanhson487 <thanhsonptitit@gmail.com>
2021-04-01 17:33:29 +0700

    f

--- control.js
+++ control.js
@@ -1,38 +1,64 @@
 $(document).ready(function () {
-  $("taisan").css({ "background-color": "#2F3240", color: "#ED8F21" });
-  $("#danhmuc").css({ "background-color": "#131722", color: "#ffffff" });
-  $("#solenh").css({ "background-color": "#131722", color: "#ffffff" });
-  $(".datachangesolenh").hide();
-  $(".datachangeMenu").hide();
+  var checkshow = false;
 
-  $("#solenh").click(function () {
+  // $("asset").css({ "background-color": "#2F3240", color: "#ED8F21" });
+  // $("#listfooter").css({ "background-color": "#131722", color: "#ffffff" });
+  // $("#orderBook").css({ "background-color": "#131722", color: "#ffffff" });
+  // $(".datachangeorderBook").hide();
+  // $(".main__right").show();
+  // $(".datachangeMenu").hide();
+
+  $("#orderBook").click(function () {
     $(this).css({ "background-color": "#2F3240", color: "#ED8F21" });
-    $("#danhmuc").css({ "background-color": "#131722", color: "#ffffff" });
-    $("#taisan").css({ "background-color": "#131722", color: "#ffffff" });
-    $("#datachangetaisan").hide();
-    $(".datachangesolenh").show();
+    $("#listfooter").css({ "background-color": "#131722", color: "#ffffff" });
+    $("#asset").css({ "background-color": "#131722", color: "#ffffff" });
+
+    $(".main").css({
+      "grid-template-columns": "1fr 398px",
+    });
+    $(".main__right").css({ display: "flex" });
+    $(".data1").show();
+
+    $(".datachangeorderBook").show();
+    $("#datachangeasset").hide();
     $(".datachangeMenu").hide();
   });
 
-  $("#danhmuc").click(function () {
+  $("#listfooter").click(function () {
     $(this).css({ "background-color": "#2F3240", color: "#ED8F21" });
-    $("#solenh").css({ "background-color": "#131722", color: "#ffffff" });
-    $("#taisan").css({ "background-color": "#131722", color: "#ffffff" });
-    $("#datachangetaisan").hide();
-    $(".datachangesolenh").hide();
+    $("#orderBook").css({ "background-color": "#131722", color: "#ffffff" });
+    $("#asset").css({ "background-color": "#131722", color: "#ffffff" });
+
+    $(".main").css({
+      "grid-template-columns": "1fr 398px",
+    });
+    $(".main__right").css({ display: "flex" });
+
+    $(".data1").show();
+
+    $(".datachangeorderBook").hide();
+    $("#datachangeasset").hide();
     $(".datachangeMenu").show();
   });
 
-  $("#taisan").click(function () {
+  $("#asset").click(function () {
     $(this).css({ "background-color": "#2F3240", color: "#ED8F21" });
-    $("#solenh").css({ "background-color": "#131722", color: "#ffffff" });
-    $("#danhmuc").css({ "background-color": "#131722", color: "#ffffff" });
-    $("#datachangetaisan").show();
-    $(".datachangesolenh").hide();
+    $("#orderBook").css({ "background-color": "#131722", color: "#ffffff" });
+    $("#listfooter").css({ "background-color": "#131722", color: "#ffffff" });
+
+    $(".main__right").show();
+    $(".main").css({
+      "grid-template-columns": "1fr 398px",
+    });
+    $(".main__right").css({ display: "flex" });
+
+    $(".datachangeorderBook").hide();
+    $("#datachangeasset").show();
     $(".datachangeMenu").hide();
   });
 
   $(".hideTabRight").click(function () {
+    checkshow = true;
     $(".main__right").hide();
     $(".main").css({
       "grid-template-columns": "1fr",
@@ -40,43 +66,15 @@ $(document).ready(function () {
     $(".footer--scare").css({
       display: "block",
     });
+
+    $("#asset").css({ "background-color": "#2F3240", color: "#131722" });
+    $("#orderBook").css({ "background-color": "#2F3240", color: "#131722" });
+    $("#listfooter").css({ "background-color": "#2F3240", color: "#131722" });
+
+    $("#orderBook").css({ "background-color": "#131722", color: "#ffffff" });
+    $("#listfooter").css({ "background-color": "#131722", color: "#ffffff" });
+    $("#asset").css({ "background-color": "#131722", color: "#ffffff" });
   });
 
   // action show
-  $(".solenh").click(function () {
-    $(".main__right").show();
-
-    $(".footer--scare").css({
-      display: "none",
-    });
-    $(".main").css({
-      "grid-template-columns": "1fr 398px",
-    });
-
-    $("#solenh").trigger("click");
-  });
-
-  $(".danhmuc").click(function () {
-    $(".main__right").show();
-
-    $(".footer--scare").css({
-      display: "none",
-    });
-    $(".main").css({
-      "grid-template-columns": "1fr 398px",
-    });
-    $("#danhmuc").trigger("click");
-  });
-
-  $(".taisan").click(function () {
-    $(".main__right").show();
-
-    $(".footer--scare").css({
-      display: "none",
-    });
-    $(".main").css({
-      "grid-template-columns": "1fr 398px",
-    });
-    $("#taisan").trigger("click");
-  });
 });
